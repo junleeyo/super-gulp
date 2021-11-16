0e9d57cc0d7b26ffa4388bfead7283ba88c12103
junleeyo <lcj715@korea.com>
2021-11-16 16:17:02 +0900

    end2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,7 @@ import autop from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 const sass = gulpSass(nodeSass);
 
@@ -40,34 +41,39 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-    const img = () =>
+const img = () =>
     gulp
     .src(routes.img.src)
     .pipe(image())
     .pipe(gulp.dest(routes.img.dest));    
 
 const styles = () => 
-  gulp
-  .src(routes.scss.src)
-  .pipe(sass().once("error", sass.logError))
-  .pipe(autop({
-    browsers: ["last 2 versions"]
-  }))
-  .pipe(miniCSS())
-  .pipe(gulp.dest(routes.scss.dest));
+    gulp
+    .src(routes.scss.src)
+    .pipe(sass().once("error", sass.logError))
+    .pipe(autop({
+      browsers: ["last 2 versions"]
+    }))
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
 
 const js = () =>
+    gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ['@babel/preset-env'] }),
+          [ 'uglifyify', { global: true } ]
+        ]
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
+const gh = () => 
   gulp
-  .src(routes.js.src)
-  .pipe(
-    bro({
-      transform: [
-        babelify.configure({ presets: ['@babel/preset-env'] }),
-        [ 'uglifyify', { global: true } ]
-      ]
-    })
-  )
-  .pipe(gulp.dest(routes.js.dest));
+  .src("build/**/*")
+  .pipe(ghPages());  
 
 const watch = () => {
    gulp.watch(routes.pug.watch, pug);
@@ -76,7 +82,7 @@ const watch = () => {
    gulp.watch(routes.js.watch, js);
 };
     
-const clean = () => del(["build/"]);
+const clean = () => del(["build/", ".publish/"]);
 const prepare = gulp.series([clean, img]);
 const assets = gulp.series([pug, styles, js]);
 
@@ -84,4 +90,6 @@ const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
